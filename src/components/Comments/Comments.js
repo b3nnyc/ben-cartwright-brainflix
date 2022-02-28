749fab7f8b0801e296ac16e81aad2ef559c8cf61
Ben Cartwright <ben.cartwright@outlook.com>
2022-02-27 20:36:22 -0500

    Completed styling for comment section

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,37 +1,44 @@
 import timestampToDate from "timestamp-to-date";
+import avatar from "../../assets/images/Mohan-muruge.jpg";
+import blank_avatar from "../../assets/images/blank_avatar.png";
 import "./Comments.scss";
 
 function Comments({ comments }) {
   const count = comments.length;
   return (
-    <section className="comments">
-      <h3 className="comments__counter">{count} Comments</h3>
-      <div className="newComment">
-        <div className="newComment__avatar"></div>
-        <input
-          className="newComment__search"
-          type="text"
-          title="JOIN THE CONVERSATION"
-          placeholder="Add a new comment"
-        ></input>
-        <button
-          className="newComment__button"
-          type="submit"
-          // onClick={() => setCount(count + 1)}
-        >
-          COMMENT
-        </button>
+    <section className="newComment">
+      <h3 className="newComment__counter">{count} Comments</h3>
+      <div className="newComment__container">
+        <img className="newComment__avatar" src={avatar}></img>
+        <div className="newComment__input">
+          <div className="newComment__form">
+            <label className="newComment__label" for="comment">
+              JOIN THE CONVERSATION
+            </label>
+            <input
+              className="newComment__form-box"
+              type="text"
+              title="JOIN THE CONVERSATION"
+              placeholder="Add a new comment"
+            ></input>
+          </div>
+          <button className="newComment__button" type="submit">
+            COMMENT
+          </button>
+        </div>
       </div>
       {comments.map((comment) => (
         <div className="pastComments" key={comment.timestamp}>
-          <div className="pastComments__avatar"></div>
-          <div className="pastComments__body">
-            <h4 className="pastComments__name">{comment.name}</h4>
-            <h5 className="pastComments__date">
-              {timestampToDate(comment.timestamp, "MM/dd/yyyy")}{" "}
-            </h5>
+          <img className="pastComments__avatar" src={blank_avatar}></img>
+          <div className="pastComments__container">
+            <div className="pastComments__body">
+              <h4 className="pastComments__name">{comment.name}</h4>
+              <h5 className="pastComments__date">
+                {timestampToDate(comment.timestamp, "MM/dd/yyyy")}{" "}
+              </h5>
+            </div>
+            <p className="pastComments__copy">{comment.comment}</p>
           </div>
-          <p className="pastComments__copy">{comment.comment}</p>
         </div>
       ))}
     </section>
